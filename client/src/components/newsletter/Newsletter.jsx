e0f0c59eac332a1a764f2479af81e73794722560
Sai Rakesh <77224879+SaiRakeshk@users.noreply.github.com>
2023-12-15 18:53:27 +0530

    Update Newsletter.jsx

--- client/src/components/newsletter/Newsletter.jsx
+++ client/src/components/newsletter/Newsletter.jsx
@@ -1,8 +1,29 @@
-import React from 'react'
-import classes from './newsletter.module.css'
-import { FiSend } from 'react-icons/fi'
+import React, { useState } from 'react';
+import classes from './newsletter.module.css';
+import { FiSend } from 'react-icons/fi';
 
 const Newsletter = () => {
+  const [isSubscribed, setIsSubscribed] = useState(false);
+  const [email, setEmail] = useState('');
+  const [showError, setShowError] = useState(false);
+  
+  const handleSubscription = () => {
+    if (email.trim() !== '') {
+      setIsSubscribed(true);
+      setShowError(false);
+    } else {
+      setShowError(true);
+    }
+  };
+
+  const closePopup = () => {
+    setIsSubscribed(false);
+  };
+
+  const handleInputChange = (e) => {
+    setEmail(e.target.value);
+  };
+
   return (
     <div className={classes.container}>
       <div className={classes.wrapper}>
@@ -11,12 +32,39 @@ const Newsletter = () => {
           <h2>Send us your email and we will do the rest!</h2>
         </div>
         <div className={classes.inputContainer}>
-          <input type="email" placeholder='Type email...' />
-          <FiSend className={classes.sendIcon} />
+          <input
+            type="email"
+            placeholder="Type email..."
+            value={email}
+            onChange={handleInputChange}
+          />
+          <button onClick={handleSubscription} className={classes.sendButton}>
+            <FiSend className={classes.sendIcon} />
+          </button>
         </div>
+        {showError && (
+          <div className={classes.popup}>
+            <div className={classes.popupContent}>
+              <p>Please enter an email address.</p>
+              <span className={classes.close} onClick={() => setShowError(false)}>
+                &times;
+              </span>
+            </div>
+          </div>
+        )}
+        {isSubscribed && (
+          <div className={classes.popup}>
+            <div className={classes.popupContent}>
+              <p>Subscribed successfully!</p>
+              <span className={classes.close} onClick={closePopup}>
+                &times;
+              </span>
+            </div>
+          </div>
+        )}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Newsletter
+export default Newsletter;
